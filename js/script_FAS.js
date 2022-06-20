a58bf713af5b42f47d80f40acd1360921bba51ee
Fernando Amarilho Silveira <famarilhosilveira@gmail.com>
2022-06-20 12:00:36 -0300

    finaliza jogo mario

--- js/script_FAS.js
+++ js/script_FAS.js
@@ -7,16 +7,27 @@ const jump = () => {
     setTimeout(() => {
         mario.classList.remove("jump-mario");
     }, 500);
-}; 
+};
 
 const loopGame = setInterval(() => {
-    pipe.style.animation = "none";
-    pipe.style.left = `${pipePosition}px`;
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window
+        .getComputedStyle(mario)
+        .bottom.replace("px", "");
 
-    mario.style.animation = "none";
-    mario.style.bottom = `${marioPosition}px`;
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = "none";
+        pipe.style.left = `${pipePosition}px`;
 
-    
-})
+        mario.style.animation = "none";
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = "./Images/mario-game-over.png";
+        mario.style.width = "75px";
+        mario.style.marginLeft = "45px";
+
+        clearInterval(loopGame);
+    }
+}, 10);
 
 document.addEventListener("keydown", jump);
